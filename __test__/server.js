bec97386e4f6c8202ca801ec068234d4f2440f46
icepy <xiangwenwe@foxmail.com>
2018-05-06 01:17:28 +0800

    增加ticket和user接口

--- __test__/server.js
+++ __test__/server.js
@@ -2,12 +2,12 @@ const http = require('http')
 const DT = require('../index');
 
 const env = process.env
+
 const c = DT.createDTCompanyClient({
-  CorpID: env['CorpID'],
+  CorpID: env['CorpId'],
   CorpSecret: env['CorpSecret'],
 })
 c.refreshCompanyAccessToken()
-
 // http.createServer(function(req, res){
 
 // }).listen(8083)
